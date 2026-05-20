97053dbe464735eef2f3670b687b8729f1dd4c8e
JOliverosRIng <150499716+JOliverosRIng@users.noreply.github.com>
2026-05-19 21:17:48 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/AppDeAgendamientos">
-    <App />
-  </BrowserRouter>
-)
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App.jsx";
+
+import ThemeProvider from "./context/ThemeContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+
+    <ThemeProvider>
+
+      <App />
+
+    </ThemeProvider>
+
+  </StrictMode>
+);
